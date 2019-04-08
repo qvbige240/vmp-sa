6b3c6bc931b11d02c728309f2afdea0a40b30d28
zouqing <qing.zou@timanetworks.com>
2019-04-08 14:50:25 +0800

    main args config

--- app/main.c
+++ app/main.c
@@ -6,6 +6,7 @@
  */
 
 #include <signal.h>
+#include <getopt.h>
 
 #include <bll.h>
 
@@ -18,7 +19,17 @@ void ignore_sigpipe(void)
 	}
 }
 
-int main(void)
+static void help(char *progname)
+{
+	fprintf(stderr, "------------------------------------------------------------------\n");
+	fprintf(stderr, "Usage: %s\n" \
+					" [-h |  --help ]\tdisplay this help\n" \
+					" [-c |  --conf ]\t\tconf file\n" \
+					, progname);
+	fprintf(stderr, "------------------------------------------------------------------\n");
+}
+
+int main(int argc, char* argv[])
 {
 #if 0
 	ignore_sigpipe();
@@ -36,8 +47,36 @@ int main(void)
 	//sigaction(SIGFPE,  &action, NULL);
 #endif
 
+	char* config_file = NULL;
 
-	bll_init("./tima/tvmpssd.conf");
+	static const struct option long_options[] = {
+		{ "help",			no_argument,			NULL, 'h' },
+		{ "conf",			required_argument,		NULL, 'c' },
+		{ NULL, 0, NULL, 0 }
+	};
+
+	optind = 1;
+	int o;
+	while ((o = getopt_long(argc, argv, "c:h", long_options, NULL)) >= 0) {
+		switch(o) {
+			case 'c':
+				config_file = strdup(optarg);
+				printf("config-file = %s\n", config_file);
+				break;
+			default:
+				help(argv[0]);
+				return 0;
+		}
+	}
+
+	if(config_file == NULL || strlen(config_file) <= 0)
+	{
+		printf("Need conf file\n");
+		help(argv[0]);
+		return -1;
+	}
+
+	bll_init(config_file);
 	
 	while (bll_cond())
 	{
