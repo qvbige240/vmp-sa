0b666faa436f1860e5d451b56942ff2a0d281873
zouqing <qing.zou@timanetworks.com>
2019-05-30 09:34:36 +0800

    add socket ioa

---
+++ include/mw/support/tima_sockioa.h
@@ -0,0 +1,47 @@
+/**
+ * History:
+ * ================================================================
+ * 2019-05-29 qing.zou created
+ *
+ */
+
+#ifndef TIMA_SOCKIOA_H
+#define TIMA_SOCKIOA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <pthread.h>
+
+#include <event2/event.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+typedef struct _VmpSocketIOA
+{
+	vpk_socketio_t			abs;
+	int						bound;
+
+	vpk_sockaddr			peer_addr;
+	vpk_sockaddr			local_addr;
+	struct bufferevent	   *bev;
+
+	struct event		   *read_event;
+
+} VmpSocketIOA;
+
+VmpSocketIOA* vmp_unbound_relay_socket_create(void *e, int family, vpk_prototype_t type, sock_application_t atype);
+int vmp_relay_socket_create(void *e, sock_application_t atype, VmpSocketIOA **s);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TIMA_SOCKIOA_H
---
+++ mw/support/tima_sockioa.c
@@ -0,0 +1,60 @@
+/**
+ * History:
+ * ================================================================
+ * 2019-05-29 qing.zou created
+ *
+ */
+
+#include "vmp.h"
+#include "tima_typedef.h"
+#include "tima_sockioa.h"
+
+
+VmpSocketIOA* vmp_unbound_relay_socket_create(void *e, int family, vpk_prototype_t type, sock_application_t atype)
+{
+	VmpSocketIOA *sock = VPK_CALLOC(1, sizeof(VmpSocketIOA));
+	if (sock)
+	{
+		vpk_socket_ioa_create(e, family, type, &sock->abs);
+
+		sock->abs.apptype = atype;
+	}
+
+	return sock;
+}
+
+void vmp_socket_release(VmpSocketIOA *s)
+{
+
+}
+
+int vmp_relay_socket_create(void *e, sock_application_t atype, VmpSocketIOA **s)
+{
+	int ret = 0;
+	VmpSocketIOA *sock = NULL;
+	vpk_sockaddr local_addr = {0};
+
+	local_addr.s4.sin_family = PF_INET;
+	local_addr.s4.sin_port = htons(13000);		//...
+	local_addr.s4.sin_addr.s_addr = INADDR_ANY;
+
+	sock = vmp_unbound_relay_socket_create(e, PF_INET, VPK_PROTOTYPE_UDP, atype /*VPK_APPTYPE_WEBSOCKET_RELAY*/);
+	if (!sock) {
+		VMP_LOGE("create relay socket error!");
+		return -1;
+	}
+
+	ret = vpk_addr_bind(sock->abs.fd, &local_addr, 0);
+	if (ret >= 0) {
+		sock->bound = 1;
+	} else {
+		VMP_LOGE("socket bind error!");
+		vmp_socket_release(sock);
+		return -1;
+	}
+
+	*s = sock;
+
+	return 0;
+}
+
